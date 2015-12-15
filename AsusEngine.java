4e9df6517a5546460e6db409420400da1bb299f3
Andre Susanto <andre@andresusanto.info>
2015-12-15 09:07:18 +0700

    init

---
+++ AsusEngine.java
@@ -0,0 +1,168 @@
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.*;
+import org.apache.hadoop.mapreduce.*;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.*;
+import org.apache.log4j.Logger;
+
+import javax.xml.stream.*;
+import java.io.*;
+
+import java.util.List;
+import java.util.LinkedList;
+import java.util.LinkedHashMap;
+import java.util.HashMap;
+import java.util.Comparator;
+import java.util.Collections;
+
+import static javax.xml.stream.XMLStreamConstants.*;
+
+public final class AsusEngine {
+	
+	private static final Logger log = Logger.getLogger(AsusEngine.class);
+
+	public static class Map extends Mapper<LongWritable, Text, Text, IntWritable> {
+		private final static IntWritable one = new IntWritable(1);
+		private Text author = new Text();
+
+		@Override
+		protected void map(LongWritable key, Text value, Mapper.Context context) throws IOException, InterruptedException {
+			String document = value.toString();
+			try {
+				XMLStreamReader reader = XMLInputFactory.newInstance().createXMLStreamReader(new ByteArrayInputStream(document.getBytes()));
+				String currentElement = "";
+				while (reader.hasNext()) {
+					int code = reader.next();
+					switch (code) {
+						case START_ELEMENT:
+							currentElement = reader.getLocalName();
+							break;
+						case CHARACTERS:
+							if (currentElement.equalsIgnoreCase("author")) {
+								String auth = reader.getText();
+								if (auth.length() > 1){
+									author.set(auth);
+									context.write(author, one);
+								}
+							}
+						break;
+					}
+				}
+				reader.close();
+				author.set("_DOCUMENT_"); // count dokumen
+				context.write(author, one);
+			} catch (Exception e) {
+				log.error("Error processing '" + document + "'", e);
+			}
+		}
+	}
+	
+    public static class Reduce extends Reducer<Text, IntWritable, Text, IntWritable> {
+
+        private HashMap<Text, IntWritable> countMap = new HashMap<>();
+		
+		@Override
+        public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
+
+            int sum = 0;
+            for (IntWritable val : values) {
+                sum += val.get(); 
+            }
+			
+			// cari kvalue terkecil di countMap
+			Text minKey = null; int minVal = 0;
+			for (Text key1 : countMap.keySet()) {
+				if (minVal < countMap.get(key1).get()){
+					minVal = countMap.get(key1).get();
+					minKey = key1;
+				}
+            }
+			
+			// kalo misalnya jumlah artikel yg d tulis penulis ini > artikel minimal di countMap
+			if (sum > minVal){
+				if (minKey != null && countMap.size() > 6){
+					countMap.remove(minKey); // hapus kalo misalnya countmap masih belom penuh (disini kapasitas = 7)
+				}
+				countMap.put(new Text(key), new IntWritable(sum));
+			}
+
+        }
+
+        @Override
+        protected void cleanup(Context context) throws IOException, InterruptedException {
+			HashMap<Text, IntWritable> sortedMap = sortByValues(countMap);
+
+            int counter = 0;
+            for (Text key : sortedMap.keySet()) {
+                if (counter++ == 6) {
+                    break;
+                }
+				
+                context.write(key, sortedMap.get(key));
+            }
+			
+        }
+    }
+
+	public static void main(String... args) {
+		try{
+			runJob(args[0], args[1]);
+		}catch(Exception e){
+			log.error("Error MAIN", e);
+		}
+	}
+
+	public static void runJob(String input, String output) throws Exception {
+		Configuration conf = new Configuration();
+		conf.set("key.value.separator.in.input.line", " ");
+		conf.set("xmlinput.start", "article");
+		conf.set("xmlinput.end", "/article");
+
+		Job job = new Job(conf);
+		job.setJarByClass(AsusEngine.class);
+		job.setMapperClass(Map.class);
+		job.setReducerClass(Reduce.class);
+		
+		job.setOutputKeyClass(Text.class);
+		job.setOutputValueClass(Text.class);
+		
+		job.setInputFormatClass(XmlInputFormat.class);
+		
+		job.setOutputKeyClass(Text.class); 
+        job.setOutputValueClass(IntWritable.class); 
+		
+		//job.setNumReduceTasks(0);
+		//job.setOutputFormatClass(TextOutputFormat.class);
+
+		FileInputFormat.setInputPaths(job, new Path(input));
+		Path outPath = new Path(output);
+		FileOutputFormat.setOutputPath(job, outPath);
+		outPath.getFileSystem(conf).delete(outPath, true);
+
+		job.waitForCompletion(true);
+	}
+	
+	
+	private static <K extends Comparable, V extends Comparable> HashMap<K, V> sortByValues(HashMap<K, V> HashMap) {
+        List<HashMap.Entry<K, V>> entries = new LinkedList<HashMap.Entry<K, V>>(HashMap.entrySet());
+
+        Collections.sort(entries, new Comparator<HashMap.Entry<K, V>>() {
+
+            @Override
+            public int compare(HashMap.Entry<K, V> o1, HashMap.Entry<K, V> o2) {
+                return o2.getValue().compareTo(o1.getValue());
+            }
+        });
+
+        //LinkedHashHashMap will keep the keys in the order they are inserted
+        //which is currently sorted on natural ordering
+        HashMap<K, V> sortedHashMap = new LinkedHashMap<K, V>();
+
+        for (HashMap.Entry<K, V> entry : entries) {
+            sortedHashMap.put(entry.getKey(), entry.getValue());
+        }
+
+        return sortedHashMap;
+    }
+}
---
+++ XmlInputFormat.java
@@ -0,0 +1,205 @@
+// XmlInputFormat
+// (C) 2015 by alexholmes and andresusanto
+// forked from alexholmes
+// modified by andresusanto in order to make it work with DBLP
+
+
+import java.nio.charset.StandardCharsets;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.*;
+import org.apache.hadoop.io.*;
+import org.apache.hadoop.mapreduce.*;
+import org.apache.hadoop.mapreduce.lib.input.*;
+import org.slf4j.*;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Arrays;
+
+/**
+ * Reads records that are delimited by a specific begin/end tag.
+ */
+public class XmlInputFormat extends TextInputFormat {
+
+  private static final Logger log =
+      LoggerFactory.getLogger(XmlInputFormat.class);
+
+  public static final String START_TAG_KEY = "xmlinput.start";
+  public static final String END_TAG_KEY = "xmlinput.end";
+
+  @Override
+  public RecordReader<LongWritable, Text> createRecordReader(
+      InputSplit split, TaskAttemptContext context) {
+    try {
+      return new XmlRecordReader((FileSplit) split,
+          context.getConfiguration());
+    } catch (IOException ioe) {
+      log.warn("Error while creating XmlRecordReader", ioe);
+      return null;
+    }
+  }
+
+  /**
+   * XMLRecordReader class to read through a given xml document to
+   * output xml blocks as records as specified
+   * by the start tag and end tag
+   */
+  public static class XmlRecordReader
+      extends RecordReader<LongWritable, Text> {
+
+    private final byte[] startTag;
+    private final byte[] endTag;
+    private final long start;
+    private final long end;
+    private final FSDataInputStream fsin;
+    private final DataOutputBuffer buffer = new DataOutputBuffer();
+    private LongWritable currentKey;
+    private Text currentValue;
+
+    public XmlRecordReader(FileSplit split, Configuration conf)
+        throws IOException {
+      startTag = conf.get(START_TAG_KEY).getBytes("UTF-8");
+      endTag = conf.get(END_TAG_KEY).getBytes("UTF-8");
+
+      // open the file and seek to the start of the split
+      start = split.getStart();
+      end = start + split.getLength();
+      Path file = split.getPath();
+      FileSystem fs = file.getFileSystem(conf);
+      fsin = fs.open(split.getPath());
+      fsin.seek(start);
+    }
+
+    private boolean next(LongWritable key, Text value)
+        throws IOException {
+      if (fsin.getPos() < end && readUntilMatch(startTag, false)) {
+        try {
+          //buffer.write(startTag);
+          if (readUntilMatch(endTag, true)) {
+            key.set(fsin.getPos());
+            value.set(buffer.getData(), 0, buffer.getLength());
+			//throw new IOException(new String(buffer.getData(), StandardCharsets.UTF_8));
+            return true;
+          }
+        } finally {
+          buffer.reset();
+        }
+      }
+      return false;
+    }
+
+    @Override
+    public void close() throws IOException {
+      fsin.close();
+    }
+
+    @Override
+    public float getProgress() throws IOException {
+      return (fsin.getPos() - start) / (float) (end - start);
+    }
+
+    private boolean readUntilMatch(byte[] match, boolean withinBlock) throws IOException {
+		StringBuilder sb = new StringBuilder();
+		String allMatchs = new String(match, StandardCharsets.UTF_8);
+		List matches = Arrays.asList(allMatchs.split("\\|"));
+		
+		
+		int state = 0;
+		// STATE menjelaskan posisi/status dari LOOP
+		// STATE == 0, loop baru dimulai, cari '<' sampai ketemu
+		// STATE == 1, loop sudah menemukan '<', lanjutkan membaca sampai menemukan spasi. Lalu periksa apakah ada yg match
+		//				jika ada yg match, lanjut ke state == 2, jika tidak loop sampai ketemu '>', lalu reset state menjadi 0
+		// STATE == 2, ada yang match, lakukan loop sampai ketemu '>' lalu return true
+			
+		while (true) {
+			int b = fsin.read();
+			char bc = (char) b;
+			
+			// penanganan karakter bhs jerman dsb
+			if (bc == '&' || bc == ';') {
+				b = 32;
+				bc = ' ';
+			}
+			
+			// buffer data yang diperlukan
+			if (withinBlock) {
+				buffer.write(b);
+			}
+					
+			switch(state){
+				case 0:
+					if (b == -1) {
+						return false;
+					}
+					
+					if (bc == '<'){
+						state = 1;
+					}else if (!withinBlock && fsin.getPos() >= end) {
+						return false;
+					}
+					
+					break;
+				case 1:
+					if (bc != ' ' && bc != '>'){
+						sb.append(bc);
+					}else{
+						String tag = sb.toString();
+						if (matches.contains(tag)){		
+							if (bc == '>'){
+								return true;
+							}
+							state = 2;
+						}else{
+							state = 0;
+						}
+						sb.setLength(0);
+						
+						if (!withinBlock && state == 2){
+							buffer.write('<');
+							buffer.write(tag.getBytes());
+							buffer.write('>');
+						}
+						
+						
+						//throw new IOException("COMPARE " + tag + " IN " + matches.size());
+					}
+					break;
+				case 2:
+					if (bc == '>'){
+						return true;
+					}
+					break;
+			}
+			
+			
+		}
+		
+    }
+
+    @Override
+    public LongWritable getCurrentKey()
+        throws IOException, InterruptedException {
+      return currentKey;
+    }
+
+    @Override
+    public Text getCurrentValue()
+        throws IOException, InterruptedException {
+      return currentValue;
+    }
+
+    @Override
+    public void initialize(InputSplit split,
+                           TaskAttemptContext context)
+        throws IOException, InterruptedException {
+    }
+
+    @Override
+    public boolean nextKeyValue()
+        throws IOException, InterruptedException {
+      currentKey = new LongWritable();
+      currentValue = new Text();
+      return next(currentKey, currentValue);
+    }
+  }
+}
